38e462bcab687145a3ea04c23e33b1d233114891
Henrique <coelho.henrique0707@gmail.com>
2019-12-15 19:53:20 -0300

    ajustes no código backend

--- agenda/src/main/java/br/com/agenda/model/Cliente.java
+++ agenda/src/main/java/br/com/agenda/model/Cliente.java
@@ -12,16 +12,49 @@ public class Cliente extends AbstractEntity
 	private String nome;
 	@CPF
 	private String cpf;
-//	@OneToMany(mappedBy = "cliente")
-//	private Set<Email> emails;
 	
+	//Telefones
+	private String telFixo;
+	private String telCelular;
+	
+	//Email
+	private String email;
+	
+	//Endereço Cliente
+	private String cep;
+	private String logradouro;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	
+
 		
 	//GETTERS SETTERS
 	public String getNome() 		           {return nome;}
 	public void setNome(String nome)           {this.nome = nome;}
 	public String getCpf() 			           {return cpf;}
 	public void setCpf(String cpf)             {this.cpf = cpf;}
-//	public Set<Email> getEmails()              {return emails;}
-//	public void setEmails(Set<Email> emails)   {this.emails = emails;}
+	
+	//GETTERS SETTERS ENDEREÇO
+	public String getCep()                       {return cep;}
+	public void setCep(String cep)               {this.cep = cep;}
+	public String getLogradouro()                {return logradouro;}
+	public void setLogradouro(String logradouro) {this.logradouro = logradouro;}
+	public String getBairro()                    {return bairro;}
+	public void setBairro(String bairro)         {this.bairro = bairro;}
+	public String getCidade()                    {return cidade;}
+	public void setCidade(String cidade)         {this.cidade = cidade;}
+	public String getUf()                        {return uf;}
+	public void setUf(String uf)                 {this.uf = uf;}
+	
+	//GETTERS SETTERS Telefones
+	public String getTelFixo()                   {return telFixo;}
+	public void setTelFixo(String telFixo)       {this.telFixo = telFixo;}
+	public String getTelCelular()                {return telCelular;}
+	public void setTelCelular(String telCelular) {this.telCelular = telCelular;}
+	
+	//GETTERS SETTERS Email
+	public String getEmail()           {return email;}
+	public void setEmail(String email) {this.email = email;}
 	
 }
